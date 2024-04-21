cebe3f8ced54bda6f77438ecb92b05218aeddb09
Ali Ameur <aliameur169@gmail.com>
2024-04-21 18:02:24 +0100

    feat: add cactus mode and remove unused labels

--- config.py
+++ config.py
@@ -3,29 +3,31 @@ from typing import Tuple
 
 class Config:
 
-    def __init__(self, left: Tuple[str, ...], right: Tuple[str, ...], up: Tuple[str, ...], down: Tuple[str, ...],
-                 label: str):
+    def __init__(self, left: Tuple[str, ...], right: Tuple[str, ...], up: Tuple[str, ...], down: Tuple[str, ...]):
         self.left = left
         self.right = right
         self.up = up
         self.down = down
-        self.label = label
 
     @classmethod
     def get_melon_pumpkin(cls):
-        return cls(('x', 'a'), ('x', 'd'), ('x', 'w'), ('x', 's'), "Melon / Pumpkin")
+        return cls(('x', 'a'), ('x', 'd'), ('w',), ('s',))
 
     @classmethod
     def get_cocoa(cls):
-        return cls(('a',), ('d',), ('x', 'w'), ('x', 's'), "Cocoa Beans")
+        return cls(('a',), ('d',), ('x', 'w'), ('x', 's'))
 
     @classmethod
     def get_mushroom(cls):
-        return cls(('x', 'w', 'a'), ('x', 'd'), ('w',), ('s',), "Mushroom")
+        return cls(('x', 'w', 'a'), ('x', 'd'), ('w',), ('s',))
 
     @classmethod
     def get_sugarcane(cls):
-        return cls(('x', 'a'), ('x', 's'), ('w',), ('s',), "Sugarcane")
+        return cls(('x', 'a'), ('x', 's'), ('w',), ('s',))
+
+    @classmethod
+    def get_cactus(cls):
+        return cls(('x', 'a'), ('x', 'd'), ('w',), ('s',))
 
     @classmethod
     def get_modes(cls):
@@ -34,4 +36,5 @@ class Config:
             "Cocoa Beans": cls.get_cocoa,
             "Mushroom": cls.get_mushroom,
             "Sugarcane": cls.get_sugarcane,
+            "Cactus": cls.get_cactus,
         }
