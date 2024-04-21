299f18b311c995aa11b2f41c8c25ad80aff8b606
Ali Ameur <aliameur169@gmail.com>
2024-04-21 09:16:22 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,128 @@
+from threading import Lock
+from PySide6 import QtWidgets
+from pynput.keyboard import Controller, Listener, Key
+
+from config import Config
+
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.modes = Config.get_modes()
+
+        self.setup_ui()
+
+        self.macro_running = False
+
+        self.config = Config.get_melon_pumpkin()
+
+        self.keyboard = Controller()
+        self.lock = Lock()
+        self.holding_keys = []
+
+    def setup_ui(self):
+        self.setWindowTitle("Hypixel Farming Macro")
+        self.resize(280, 150)
+
+        self.main_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.main_widget)
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.mode_group = QtWidgets.QGroupBox("Select Mode:")
+        self.mode_layout = QtWidgets.QVBoxLayout()
+        self.mode_radios = {}
+
+        for i, label in enumerate(self.modes):
+            mode_radio = QtWidgets.QRadioButton(label)
+            self.mode_layout.addWidget(mode_radio)
+            self.mode_radios[label] = mode_radio
+
+            if i == 0:
+                mode_radio.setChecked(True)
+
+        self.mode_group.setLayout(self.mode_layout)
+        self.layout.addWidget(self.mode_group)
+
+        self.toggle_button = QtWidgets.QPushButton('Start', self)
+        self.layout.addWidget(self.toggle_button)
+        self.main_widget.setLayout(self.layout)
+
+        self.toggle_button.clicked.connect(self.toggle_macro)
+
+        self.show()
+
+    def toggle_macro(self):
+        if self.macro_running:
+            self.macro_running = False
+            self.toggle_button.setText('Start')
+        else:
+            self.macro_running = True
+            self.toggle_button.setText('Stop')
+            self.config = self.get_current_config()
+
+    def setup_keypress_listener(self):
+        '''
+        Set up asynchronous listener to detect keypress events and send
+        hold keys according to current config.
+        '''
+
+        def on_press(key: Key):
+
+            if not self.macro_running or key not in [Key.left, Key.right, Key.up, Key.down, Key.shift_r]:
+                return
+
+            try:
+                release_keys()
+
+                if key == Key.left:
+                    self.holding_keys = list(self.config.left)
+                elif key == Key.right:
+                    self.holding_keys = list(self.config.right)
+                elif key == Key.up:
+                    self.holding_keys = list(self.config.up)
+                elif key == Key.down:
+                    self.holding_keys = list(self.config.down)
+                elif key == Key.shift_r:
+                    self.holding_keys = []
+
+                hold_keys()
+                print("pressing")
+            except Exception as e:
+                print('error', e)
+
+        def on_release(key):
+            if key == Key.ctrl:
+                self.key_modpfx = ''
+            elif key == Key.cmd:
+                self.key_modpfx = ''
+
+        def hold_keys():
+            with self.lock:
+                for key in self.holding_keys:
+                    self.keyboard.press(key)
+
+        def release_keys():
+            with self.lock:
+                for key in self.holding_keys:
+                    self.keyboard.release(key)
+                self.holding_keys.clear()
+
+        self.listener = Listener(
+            on_press=on_press,
+            on_release=on_release
+        )
+        self.listener.start()
+
+    def get_current_config(self):
+        label: str = [mode.text() for mode in self.mode_radios.values() if mode.isChecked()][0]
+        config = Config.get_modes().get(label)()
+        return config
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    main = Main()
+    main.setup_keypress_listener()
+    app.exec()
+
+# https://github.com/moses-palmer/pynput/issues/511
---
+++ config.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+
+
+class Config:
+
+    def __init__(self, left: Tuple[str, ...], right: Tuple[str, ...], up: Tuple[str, ...], down: Tuple[str, ...],
+                 label: str):
+        self.left = left
+        self.right = right
+        self.up = up
+        self.down = down
+        self.label = label
+
+    @classmethod
+    def get_melon_pumpkin(cls):
+        return cls(('x', 'a'), ('x', 'd'), ('x', 'w'), ('x', 's'), "Melon / Pumpkin")
+
+    @classmethod
+    def get_cocoa(cls):
+        return cls(('a',), ('d',), ('x', 'w'), ('x', 's'), "Cocoa Beans")
+
+    @classmethod
+    def get_mushroom(cls):
+        return cls(('x', 'w', 'a'), ('x', 'd'), ('w',), ('s',), "Mushroom")
+
+    @classmethod
+    def get_sugarcane(cls):
+        return cls(('x', 'a'), ('x', 's'), ('w',), ('s',), "Sugarcane")
+
+    @classmethod
+    def get_modes(cls):
+        return {
+            "Melon / Pumpkin": cls.get_melon_pumpkin,
+            "Cocoa Beans": cls.get_cocoa,
+            "Mushroom": cls.get_mushroom,
+            "Sugarcane": cls.get_sugarcane,
+        }
